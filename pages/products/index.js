25a1b808c97ea7b2492a87db304ca05677301a09
Jan Maaß <git@antipost.de>
2023-11-29 16:03:33 +0100

    All Listings Page

---
+++ pages/products/index.js
@@ -0,0 +1,33 @@
+import useSWR from "swr";
+
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+const AllListingsPage = () => {
+  const { data, error } = useSWR('/api/products', fetcher);
+
+  if (error) {
+    return <h2>Error loading products</h2>;
+  }
+  if (!data) {
+    return <h2>Loading products...</h2>
+  }
+
+  return (
+    <div>
+      <h1>All Products</h1>
+      <ul>
+        {data.map((product) => (
+          <li key={product.id}>
+            <a href={`/products/${product.id}`}>{product.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AllListingsPage;
